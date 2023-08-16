cf417d439e201b8730b9bc5fccb7a07ab3fc5ff3
williamNyongesa <marakawilliam@gmail.com>
2023-08-16 21:42:04 +0300

    completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { username } from "../data/user";
+import { city } from "../data/user";
 function Home() {
   return (
     <div id="home">
@@ -9,3 +10,4 @@ function Home() {
     </div>
   );
 }
+export default Home
